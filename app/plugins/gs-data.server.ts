e96250c1ff0912655eb529e0537637c7c4dc2dbe
Jim <nuttapon.srpn@gmail.com>
2026-06-14 11:47:07 +0700

    fix(gs-data): update log messages for consistency in server-side initialization

--- app/plugins/gs-data.server.ts
+++ app/plugins/gs-data.server.ts
@@ -4,12 +4,12 @@ import { useGsData } from "~/composables/gs/useGsData";
 export default defineNuxtPlugin(async () => {
   const { init, initialized } = useGsData();
   if (!initialized.value) {
-    console.log("[TFT Data] Starting server-side initialization...");
+    console.log("[Gs Data] Starting server-side initialization...");
     const result = await init();
     if (result.status === "error") {
-      console.error("[TFT Data Init Error]", result.error);
+      console.error("[Gs Data Init Error]", result.error);
     } else {
-      console.log("[TFT Data] Server-side initialization successful.");
+      console.log("[Gs Data] Server-side initialization successful.");
     }
   }
 });
